299165d2b1374c6b80f8516dd94bd0d72c4d4eda
silicon-monoxide <raffy.dusty@gmail.com>
2024-10-25 15:11:50 +1100

    functional website

--- cladogram_website.py
+++ cladogram_website.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import cladogram_generator_PIL as clade_gen
 
+# the two inputs (text)
 user_input = st.text_input("input:")
 user_input_dimensions = st.text_input("image height/width (in pixels):")
-st.image("cladogram.png")
-clade_gen.run_program('[[["clade zero"]], "clade one", ["clade two", ["clade three", "clade four", [[[0]], "clade five"], "clade six"]]]', "900")
+
+#if both have been filled out
+if user_input and user_input_dimensions:
+    clade_gen.run_program(user_input, user_input_dimensions)
+    st.image("cladogram.png")
