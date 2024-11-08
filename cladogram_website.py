51318f46636f7a46826286627333d2b071a789e0
silicon-monoxide <raffy.dusty@gmail.com>
2024-11-08 12:03:42 +1100

    changed website image to a png

--- cladogram_website.py
+++ cladogram_website.py
@@ -6,9 +6,14 @@ user_input = st.text_input("input:")
 user_input_dimensions = st.text_input("image height/width (in pixels):")
 
 #dropdown menu for line width
-user_input_linew = st.selectbox("Line Width", ["Large", "Medium", "Small", "Smallest"])
+user_input_linew = st.selectbox(
+    "Line Width", 
+    ["Large", "Medium", "Small", "Smallest"]
+    )
 
 #if both have been filled out
 if user_input and user_input_dimensions:
+    # run the cladogram image generator
     clade_gen.run_program(user_input, user_input_dimensions, user_input_linew)
-    st.image("cladogram.png")
+    #display the image
+    st.image("cladogram.png", output_format="PNG")
